88e4909d2aa67330e2a5168cb01cc504d2ea3560
Assis Willys Antunes <assiswillys@gmail.com>
2015-08-10 21:05:54 -0300

    Implementando movimentos do jogador

--- core/src/br/grupointegrado/SpaceInvaders/TelaJogo.java
+++ core/src/br/grupointegrado/SpaceInvaders/TelaJogo.java
@@ -1,13 +1,18 @@
 package br.grupointegrado.SpaceInvaders;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.utils.SpriteDrawable;
 import com.badlogic.gdx.utils.viewport.FillViewport;
 
 
@@ -20,6 +25,13 @@ public class TelaJogo extends TelaBase {
     private Stage palco;
     private BitmapFont fonte;
     private Label lbpontucao;
+    private Image jogador;
+    private Texture texturaJogador;
+    private Texture texturaJogadorDireita;
+    private Texture texturaJogadorEsquerda;
+    private Boolean indoDireita;
+    private Boolean indoEsquerda;
+
 
     /**
      * Construtor padrão da tela de jogo
@@ -41,6 +53,20 @@ public class TelaJogo extends TelaBase {
 
         initFonte();
         initInformacoes();
+        initJogador();
+    }
+
+    private void initJogador() {
+        texturaJogador = new Texture("sprites/player.png");
+        texturaJogadorDireita = new Texture("sprites/player-right.png");
+        texturaJogadorEsquerda = new Texture("sprites/player-left.png");
+
+        jogador = new Image(texturaJogador);
+        //metade da tela                  //metade do jogador
+        float x = camera.viewportWidth / 2 - jogador.getWidth()/2;
+        float y = 15;
+        jogador.setPosition(x, y);
+        palco.addActor(jogador);
     }
 
     private void initFonte(){
@@ -67,12 +93,64 @@ public class TelaJogo extends TelaBase {
         Gdx.gl.glClearColor(.15f, .15f, .25f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        lbpontucao.setPosition(10,camera.viewportHeight-20 ); // X Y   viewportHeight-20 significa que ira seta na posição a 20 pixel abaixo da altura maxima da tela
-
+        lbpontucao.setPosition(10, camera.viewportHeight - 20); // X Y   viewportHeight-20 significa que ira seta na posição a 20 pixel abaixo da altura maxima da tela
+        capturaTeclas();
+        atualizarJogador(delta);
+        
         palco.act(delta); // avisa que rodou um novo quadro
         palco.draw();
+    }
 
+    /**
+     * atualiza a posição do jogador
+     * @param delta
+     */
+    private void atualizarJogador(float delta) {
+        float velocidade = 200; // velocidade do movimento do jogador
+        if(indoDireita){
+            if(jogador.getX() < camera.viewportWidth - jogador.getWidth())
+            {
+                float x = jogador.getX() + velocidade * delta;
+                float y = jogador.getY();
+                jogador.setPosition(x, y);
+            }
+        }
 
+        if(jogador.getX() > 0) {
+
+            if (indoEsquerda) {
+                float x = jogador.getX() - velocidade * delta;
+                float y = jogador.getY();
+                jogador.setPosition(x, y);
+            }
+        }
+
+        if (indoDireita){
+            //trocar imagem direita
+            jogador.setDrawable(new SpriteDrawable(new Sprite(texturaJogadorDireita)));
+        }else if (indoEsquerda){
+            //trocar imagem esquerda
+            jogador.setDrawable(new SpriteDrawable(new Sprite(texturaJogadorEsquerda)));
+        }else{
+            //trocar imagem centro
+            jogador.setDrawable(new SpriteDrawable(new Sprite(texturaJogador)));
+        }
+    }
+
+    /**
+     * Verifica se as teclas estão pressionadas
+     */
+    private void capturaTeclas() {
+        indoDireita = false;
+        indoEsquerda = false;
+
+        if(Gdx.input.isKeyPressed(Input.Keys.LEFT)){
+            indoEsquerda = true;
+        }
+
+        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
+            indoDireita = true;
+        }
     }
 
     /**
@@ -111,5 +189,8 @@ public class TelaJogo extends TelaBase {
         batch.dispose();
         palco.dispose();
         fonte.dispose();
+        texturaJogador.dispose();
+        texturaJogadorDireita.dispose();
+        texturaJogadorEsquerda.dispose();
     }
 }
